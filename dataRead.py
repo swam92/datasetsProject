83dfbd5a7a5a0074811e6cbd493c91de235b01ca
Sam Wizon <svw223@nyu.edu>
2014-11-24 19:50:30 -0500

    added string counter

--- dataRead.py
+++ dataRead.py
@@ -1,5 +1,14 @@
 import csv
+csvToArr = []
+count = 0
+current = str
 with open('data/911truth.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		print(row['title'])
+		current = row['title']
+		if current.find("CIA") != -1:
+			count=count+1
+		print(current)
+		csvToArr.append(row['title'])
+
+print count
