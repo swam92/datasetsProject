fd1afd0968030bbdc6ea5b8ff8ef3e7f723113cb
Sam Wizon <svw223@nyu.edu>
2014-12-02 17:05:25 -0500

    added functionality that tags each word with a part of speech

--- dataRead.py
+++ dataRead.py
@@ -1,13 +1,17 @@
 import csv
 import nltk
+from nltk.tokenize import word_tokenize
+import pprint
 
 words = []
-
+words_red = []
 #######################
 #put titles from csv file into array 
 #######################
 for t in csv.DictReader(open('data/911truth.csv'), delimiter=','):
     words.extend(t['title'].lower().split()) # <-----------
+
+#words_red = words[len(words)-800]
 
 csv_text = nltk.Text(words)
 #put any word here.  counts occureneces in text
@@ -25,3 +29,53 @@ with open('data/TheWire.csv', 'rb') as csvfile:
 		totalStr = totalStr + " " + tempStr
 
 print totalStr
+
+
+#############################
+#Determines part of speech for every word in the text! Below is the dictionary.
+#############################
+#CC- coordinating conjunction
+#RB- adverbs
+#IN- preposition
+#NN- noun
+#JJ- adjective
+#ADJ- adjective
+#ADP- adposition (preposition??)
+#VBZ- verb!
+###############################
+
+tokenizer = None
+tagger = None
+
+def init_nltk():
+    global tokenizer
+    global tagger
+    tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
+    tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+
+def tag(text):
+    global tokenizer
+    global tagger
+    if not tokenizer:
+        init_nltk()
+    tokenized = tokenizer.tokenize(text)
+    tagged = tagger.tag(tokenized)
+    tagged.sort(lambda x,y:cmp(x[1],y[1]))
+    return tagged
+
+def main():
+	text = totalStr
+	tagged = tag(text)    
+	l = list(set(tagged))
+	l.sort(lambda x,y:cmp(x[1],y[1]))
+	pprint.pprint(l)
+
+if __name__ == '__main__':
+    main()
+
+
+
+#train = [('attack','neg'),('terrorist','neg'),('gun','neg'), ('innovate','pos')]
+
+#classifier = nltk.NaiveBayesClassifier.train(words)
+#classifier.show_most_informative_features()
