777d8092fd0ec0f04a5daf4edcff325cfe8c9c52
YiningChen <yining21@gmail.com>
2014-12-03 12:58:17 -0500

    Create makedict.py
    file that compares headers of all csv data files to guarantee data structure is the same throughout.
    also makes dictionary that pairs subreddit id to the name of the file

---
+++ scrubScripts/makedict.py
@@ -0,0 +1,40 @@
+from csv import reader
+import os
+
+def getheaders():
+  lines = open('headers.txt','w');
+  for file in os.listdir("data"):
+    source = open('data/'+file,'r');
+    for line in reader(source):
+      newline = ''
+      for col in line:
+        newline += col + '\t'
+      newline += '\n'
+      lines.write(newline)
+      break
+
+  lines.close()
+
+def compareheaders():
+  headerfile = open('headers.txt','r')
+  headers = headerfile.readlines()
+  i = 0
+  for line in headers:
+    if line != headers[0]:
+      print(i)
+    i += 1
+  headerfile.close()
+
+def makedict():
+  filenames = open('filenames.txt','w');
+  filenames.write('subreddits = {\n')
+  for file in os.listdir("data"):
+    source = open('data/'+file,'r');
+    i=0
+    for line in reader(source):
+      if i==1:
+        filenames.write("\t'{0}': '{1}',\n".format(line[14],file.split('.',1)[0]))
+        break
+      i += 1
+  filenames.write('}')
+  filenames.close()
